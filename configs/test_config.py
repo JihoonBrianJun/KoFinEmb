722fbfa5b65bad004b72212115f08ff89bf5c68e
JihoonBrianJun <jhjun0530@snu.ac.kr>
2023-11-21 04:36:24 +0900

    Config Updated

--- configs/test_config.py
+++ configs/test_config.py
@@ -3,16 +3,24 @@ from torch.distributed.fsdp import ShardingStrategy
 
 @dataclass
 class TestConfig:
+    train_data_path: str = "dataset/train.csv"
     eval_data_path: str = "dataset/eval.csv"
     sharding_strategy: ShardingStrategy = ShardingStrategy.FULL_SHARD
-    model_name: str = "EleutherAI/polyglot-ko-3.8b"
-    enable_fsdp: bool = False
-    low_cpu_fsdp: bool = False
+    optimizer: str = "AdamW"
+    model_name: str = "EleutherAI/polyglot-ko-1.3b"
+    enable_fsdp: bool = True
+    low_cpu_fsdp: bool = True
+    run_validation: bool = True
+    batch_size_training: int = 16
+    num_epochs: int = 1
     num_workers_dataloader: int = 1
+    gamma: float = 0.85
     seed: int = 2
     val_batch_size: int = 1
+    micro_batch_size: int = 16
     save_model: bool = True
     checkpoint_root_folder: str = "model_checkpoints"
     checkpoint_folder: str = "KoFinEmbInitial"
     save_optimizer: bool = False
+    lr: float = 2e-5
     max_length: int = 512
--- configs/train_config.py
+++ configs/train_config.py
@@ -4,17 +4,20 @@ from torch.distributed.fsdp import ShardingStrategy
 @dataclass
 class TrainConfig:
     train_data_path: str = "dataset/train.csv"
+    eval_data_path: str = "dataset/eval.csv"
     sharding_strategy: ShardingStrategy = ShardingStrategy.FULL_SHARD
     optimizer: str = "AdamW"
-    model_name: str = "EleutherAI/polyglot-ko-3.8b"
+    model_name: str = "EleutherAI/polyglot-ko-1.3b"
     enable_fsdp: bool = True
     low_cpu_fsdp: bool = True
-    batch_size_training: int = 4
+    run_validation: bool = True
+    batch_size_training: int = 16
     num_epochs: int = 1
     num_workers_dataloader: int = 1
     gamma: float = 0.85
     seed: int = 2
-    micro_batch_size: int = 4
+    val_batch_size: int = 1
+    micro_batch_size: int = 16
     save_model: bool = True
     checkpoint_root_folder: str = "model_checkpoints"
     checkpoint_folder: str = "KoFinEmbInitial"
